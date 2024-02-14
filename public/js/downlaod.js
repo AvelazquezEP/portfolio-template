ed38fab1c60a50caaac9201d8ebb906b6829425f
Avelazquez <avelazquez@abogadoericprice.com>
2024-02-14 14:41:14 -0800

    FIX: the new button to download a file when someone click in the button

---
+++ public/js/downlaod.js
@@ -0,0 +1,5 @@
+
+const download = () => {
+    // url = "http://127.0.0.1:5500/public/img/lawCover.webp";
+    document.getElementById('my_iframe').src = "http://127.0.0.1:5500/public/img/lawCover.webp";
+};
